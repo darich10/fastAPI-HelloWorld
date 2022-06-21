ffd667cc81ae81015be4244d4d3a202fe32352e7
darich15 <dakariona@gmail.com>
2022-06-21 06:22:02 -0500

    validation 1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 # Fast API
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -28,3 +28,13 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+
+# Validations: Query parameters
+@app.get("/person/detail")
+def show_person(
+        name: Optional[str] = Query(None, min_length=1, max_length=50),
+        age: int = Query(...)
+):
+    return {"name": name,
+            "age": age}
