fcd30a50ddfee2a69fbe59558fcb6005f5942a5e
darich10 <dakariona@gmail.com>
2022-07-07 12:56:32 -0500

    upload files

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pydantic import EmailStr
 # Fast API
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 import uvicorn
 
 app = FastAPI()
@@ -153,3 +153,17 @@ def contact(
         ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+# Upload files
+@app.post(
+    path="/post-image",
+    status_code=status.HTTP_200_OK
+)
+def post_image(
+        image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
